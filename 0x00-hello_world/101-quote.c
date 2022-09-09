5f38763a267da0ad7bdd0cebe3bae0e3cc2674e4
precious-nmesoma <preciousub024@gmail.com>
2022-09-09 05:25:53 -0700

    Be a genius

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - main
- * Return: 0
+ * main - Entry point
+ * Return: Always 1 (sucess)
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
