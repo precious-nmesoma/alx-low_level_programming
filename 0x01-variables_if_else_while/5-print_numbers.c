f9435eb23e1c59bf9641945aabccc85ff814dbf9
precious-nmesoma <preciousuba024@gmail.com>
2022-09-11 10:56:12 -0700

    Smile at the Mirror

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,14 +2,14 @@
 /**
  * main - entry point
  *
- * Retun: always returns 0
+ * Return: always returns 0
  *
  */
 int main(void)
 {
 	int a = 0;
 
-	for (; a < 10 ; a++)
+	for (; a < 10; a++)
 		printf("%d", a);
 	putchar('\n');
 	return (0);
