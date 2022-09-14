6ee535d3fd637dc9c4b118b6e8fdaff0d6344240
precious-nmesoma <preciousuba024@gmail.com>
2022-09-14 04:21:34 -0700

    Initial commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,11 @@
  *
  */
 void print_alphabet_x10(void);
-
 {
 	int alpha1;
 	int alpha2;
 
-	for (alpha1 = 0; alpha1 <= 9; alpha1++)
+	for (alpha = 0; alpha <= 9; alpha++)
 	{
 		for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
 		{
