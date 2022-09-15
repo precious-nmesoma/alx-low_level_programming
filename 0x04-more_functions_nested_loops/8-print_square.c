9d66f5b6d1074c996ef0066dc626b01519138e67
precious-nmesoma <preciousuba024@gmail.com>
2022-09-15 06:49:52 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - function that print a square
+ * @size: size of the square
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size < 1)
+		_putchat('\n');
+}
