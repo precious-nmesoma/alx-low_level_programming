46dd6094c6c071474514df273582feaf3535d154
precious-nmesoma <preciousuba024@gmail.com>
2022-09-15 12:18:56 -0700

    fixed error

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,33 +3,23 @@
  * print_triangle - prints a triangle
  * @size: size of the triangle
  *
- * Return: Always 0.
+ * Return: Always 0
  *
  */
 void print_triangle(int size)
 {
 	int x;
 	int y;
-	int z;
-
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
 
 	for (x = 1; x <= size; x++)
 	{
-		for (y = size - x; y > 0; y--)
+		for (y = 1; y <= size; y++)
 		{
-			_putchar(' ');
-		}
-		for (z = 0; z < i; z++)
-		{
-			_putchar('#');
+			if (y <= size - x)
+				_putchar(' ');
+			else
+				_putchar('#');
 		}
 		_putchar('\n');
-		}
 	}
 }
