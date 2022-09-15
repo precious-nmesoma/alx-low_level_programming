2e4ddfe96ded31b321daebe537a3307ed4371118
precious-nmesoma <preciousuba024@gmail.com>
2022-09-15 12:32:19 -0700

    correction of letter

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_square - function that print a square
  * @size: size of the square
@@ -12,16 +11,12 @@ void print_square(int size)
 	int y;
 
 	for (y = 0; y < size; y++)
-
 	{
 		for (x = 0; x < size; x++)
-
 			_putchar('#');
-
 		_putchar('\n');
 	}
-
 	if (size < 1)
 
-		_putchat('\n');
+		_putchar('\n');
 }
