2d9fb8a7a454190e151b1fc86c95d95c4451ebbc
precious-nmesoma <preciousuba024@gmail.com>
2022-09-15 05:02:50 -0700

    smile at the mirror

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul -  function that multiplies two integers.
+ * @u: first number that will run the program
+ * @x: second number that will be tested
+ * Return: 0
+ */
+int mul(int u, int x)
+{
+	int product;
+
+	product = u * x
+	return (product);
+}
+
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 My read me file for 0x04. C - More functions, more nested loops
 Write a function that checks for a digit (0 through 9).\n
+Write a function that multiplies two integers.
