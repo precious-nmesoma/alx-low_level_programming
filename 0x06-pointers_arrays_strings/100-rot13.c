b37cb98ff432fb7d5f0113a33c2fc12d35b33d5b
precious-nmesoma <preciousuba024@gmail.com>
2022-09-23 11:45:50 -0700

    Finally

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,19 +6,24 @@
  */
 char *rot13(char *s)
 {
-	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+	int i = 0;
+	int j = 0;
+	char *l = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char *r13 = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
 
-	int i, j;
-
-	for (j = 0; s[j] != '\0'; j++) /*loop through string*/
+	while (*(s + i) != '\0')
 	{
-		i = 0;
-		while (a[i] != '\0' && s[j] != a[i]) /*loop through rot13 arr*/
-			i++;
-
-		if (s[j] == a[i]) /*if alpha matches, set to index in b arr*/
-			s[j] = b[i];
+		while (*(l + j) != '\0')
+		{
+			if (*(s + i) == *(l + j))
+			{
+				*(s + i)  = *(r13 + j);
+				break;
+			}
+			j++;
+		}
+		j = 0;
+		i++;
 	}
 	return (s);
 }
