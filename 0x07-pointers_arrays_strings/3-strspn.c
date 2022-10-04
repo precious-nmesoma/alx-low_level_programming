9007dcf0e609869348e42723511c2f23e6698769
precious-nmesoma <preciousuba024@gmail.com>
2022-10-04 05:04:02 -0700

    You can't give what you dont have

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,23 +8,17 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0, j;
-	int matches = 0;
+	unsigned int i, j;
 
-	while (s[i] != '\0') /*iterate through string*/
+	for (i = 0; s[i]; i++)
 	{
-	
-		for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
+		for (j = 0; accept[j]; j++)
 		{
-			if (s[i] == accept[j]) /*record & break at first match*/
-			{
-				matches++;
+			if (s[i] == accept[j])
 				break;
-			}
-			if (accept[j + 1] == '\0' && s[i] != accept[j])
-				return (matches);/*return if idx doesn't match*/
 		}
-		i++;
+		if (!accept[j])
+			break;
 	}
-	return (matches); /* return num if all match till end */
+	return (i);
 }
