a9eb0d5844e886123d01dffe3cdcd35b71940e1a
precious-nmesoma <preciousuba024@gmail.com>
2022-10-04 05:12:24 -0700

    Do hard things

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,18 +3,20 @@
 /**
  * _strchr - locate 1st occurrence of char in string and returns pointer there
  * @s: string to search
- * target characer
+ * @c: character to search for c
  * Return: pointer to that character in string
  */
 
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	while (s[i] != '\0' && s[i] != c) /* find match */
-		i++;
 
-	if (s[i] == c) /* if match, assign to address */
-		return (&s[i]);
-	else
-		return (NULL);
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return (0);
 }
