1efef5208a7542095e42832efb766ee07446661c
precious-nmesoma <preciousuba024@gmail.com>
2022-10-04 07:07:55 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,32 +8,30 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **t;
-	int i;
-	int j;
+	int i, j;
+	int **a;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	t = (int **)malloc(sizeof(int *) * height);
-	if (t == NULL)
+	a = (int **)malloc(sizeof(int *) * height);
+	if (a == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		t[i] = (int *)malloc(sizeof(int) * width);
-		if (t[i] == NULL)
+		a[i] = (int *)malloc(sizeof(int) * width);
+		if (a[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
-			{
-				free(t[j]);
-			}
-			free(t);
+				free(a[j]);
+			free(a);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			a[i][j] = 0;
 		}
 	}
-
-	for (i = 0; i < height; i++)
-		t[i][j] = 0;
-
-	return (t);
+	return (a);
 }
