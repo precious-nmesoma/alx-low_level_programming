280d337eb18206b466d22ab7bbf40f3bec52f19f
precious-nmesoma <preciousuba024@gmail.com>
2022-10-11 09:17:54 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,4 @@
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -6,4 +7,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int getNumPrim(int num, int i);
 int wildcmp(char *s1, char *s2);
