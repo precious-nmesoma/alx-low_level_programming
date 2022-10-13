fe00f82bd56c38b00b07695ea22037a3f8d6134e
precious-nmesoma <preciousuba024@gmail.com>
2022-10-13 06:59:37 -0700

    Initial commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 #include <string.h>
 /**
- * get_op_func - selects the correct operation toperform
+ * get_op_func - selects the correct operation to perform
  * @s: operation to perform
  *
  * Return: pointer to the correct function
@@ -10,10 +10,10 @@ int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
-		{"%", op_mod},
-		{"/", op_div},
 		{"-", op_sub},
 		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
 		{NULL, NULL}
 	};
 	int i;
