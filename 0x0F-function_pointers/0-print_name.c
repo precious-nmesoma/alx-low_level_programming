c612588664ff4a13f1a35053f634a4c924e854d9
precious-nmesoma <preciousuba024@gmail.com>
2022-10-14 14:09:13 -0700

    Call me by my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,11 @@
  * print_name - function that prints a name.
  * @name: name of person
  * @p:function to print
+ *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *));
 {
-	if (name != NULL && p != NULL)
-		p(name);
+	if (name != NULL && f != NULL)
+		 f(name);
 }
